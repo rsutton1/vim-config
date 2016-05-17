869e5d1b131137b811cc57e0bd787241c12cd4a4
Rob Sutton <rsutton@rjmetrics.com>
2016-05-17 14:50:15 -0400

    Add tmux.conf file

--- setup_tmux.sh
+++ setup_tmux.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# tmux configuration
+cp _tmux.conf ~/.tmux.conf
+
+# tmux windows configuration and tmux launch script
 cp _tmux-windows.json ~/.tmux-windows.json
 chmod 0755 tn
-ln -s $(pwd)/tn /usr/local/bin/tn
+ln -sf $(pwd)/tn /usr/local/bin/tn
