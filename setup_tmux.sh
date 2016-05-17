66aa60cdfa65a068b6ea095e552f2cf0e43a3355
Rob Sutton <rsutton@rjmetrics.com>
2016-05-17 15:10:20 -0400

    Save overwritten config files into backup

--- setup_tmux.sh
+++ setup_tmux.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-# tmux configuration
-cp _tmux.conf ~/.tmux.conf
+# create backup directory using a timestamp for the current time
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+backupdir=$(pwd)/backup/$timestamp
+mkdir -p $backupdir
 
-# tmux windows configuration and tmux launch script
-cp _tmux-windows.json ~/.tmux-windows.json
+# tmux configuration
+mv ~/.tmux.conf $backupdir
+ln -s $(pwd)/_tmux.conf ~/.tmux.conf
+
+# tmux windows configuration
+mv ~/.tmux-windows.json $backupdir
+ln -s $(pwd)/_tmux-windows.json ~/.tmux-windows.json
+
+# tmux launch script
+mv /usr/local/bin/tn $backupdir
 chmod 0755 tn
-ln -sf $(pwd)/tn /usr/local/bin/tn
+ln -s $(pwd)/tn /usr/local/bin/tn
