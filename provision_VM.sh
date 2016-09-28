d604563bf5d2de412bb224a0b420152adb877775
Rob Sutton <rsutton@rjmetrics.com>
2016-09-28 14:55:53 -0400

    Fix bug in provision scripts

--- provision_VM.sh
+++ provision_VM.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ./setup_bash.sh
-sed -i -e 's/C-a/C-e/g' _tmux.conf
 ./install_tmux.sh
 ./setup_tmux.sh
 ./setup_vim.sh
--- provision_host.sh
+++ provision_host.sh
@@ -2,5 +2,7 @@
 
 ./setup_bash.sh
 ./install_tmux.sh
+sed -i -e 's/C-e/C-a/g' _tmux.conf
+sed -i -e 's/blue/green/g' _tmux.conf
 ./setup_tmux.sh
 ./setup_vim.sh
