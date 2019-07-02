98a545b8bc8ee097ee24d2b0d58eef29a2e3a67a
Rob Sutton <robert.sutton@twosixlabs.com>
2019-07-02 11:52:57 -0400

    Simply tmux installation (tmux 2.x is now in repo)

--- install_tmux.sh
+++ install_tmux.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y python-software-properties software-properties-common
-sudo add-apt-repository -y ppa:pi-rho/dev
-sudo apt-get update
-sudo apt-get install -y tmux=2.0-1~ppa1~t
-tmux -V
+apt install tmux
